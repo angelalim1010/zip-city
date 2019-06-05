48903e863009287aec21f74f82d27d04c1201c0a
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2019-06-05 11:01:51 -0400

    adding styling

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { Component } from 'react';
 import logo from "./logo.svg";
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="searchHeader">Header</div>
-      <div className="searchBox">
-        <form>
-          <label for="zipCode">Zip Code:</label>
-          <input type="number" name="zipCode" />
-          <input type="submit" value="Search" />
-        </form>
-      </div>
-    </div>
-  );
-}
+class App extends Component {
+    render(){
+        return (
+          <div className="App">
+            <div className="searchHeader">Zipcode Search</div>
+            <br></br>
+            <div className="searchBox">
+              <Form className="form" inline>
+                <FormGroup>
+                    <Label for="zipCode">Zip Code: </Label>
+                    <Input type="number" name="zipCode" />
+                    <Input type="submit" value="Search" />
+                </FormGroup>
+              </Form>
+            </div>
+          </div>
+        );
+      }
+    }
 
 export default App;
