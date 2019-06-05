1e4bc2bf8ea92c04392b5c473c91ffee53d2fb0e
Tony Fung <tfung5@gmail.com>
2019-06-05 10:31:57 -0400

    made basic search form

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchHeader">Header</div>
+      <div className="searchBox">
+        <form>
+          <label for="zipCode">Zip Code:</label>
+          <input type="number" name="zipCode" />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
     </div>
   );
 }
