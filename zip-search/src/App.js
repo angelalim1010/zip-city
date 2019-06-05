6fc0f4a18f1164ab7fc2f1d79c3d653f68e35e9c
Tony Fung <tfung5@gmail.com>
2019-06-05 11:03:25 -0400

    Search form can fetch data and display it

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,20 +1,70 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="searchHeader">Header</div>
-      <div className="searchBox">
-        <form>
-          <label for="zipCode">Zip Code:</label>
-          <input type="number" name="zipCode" />
-          <input type="submit" value="Search" />
-        </form>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      zipCode: 10025,
+      cities: []
+    };
+  }
+
+  updateZip = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  fetchZipData = zip => {
+    console.log("this is the zip:", zip);
+    axios.get(`http://ctp-zip-api.herokuapp.com/zip/${zip}`).then(res =>
+      this.setState({
+        cities: res.data
+      })
+    );
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.fetchZipData(this.state.zipCode);
+  };
+
+  showCities = () => {
+    return (
+      <div>
+        {this.state.cities.map(city => (
+          <div className="city">
+            <div className="cityName">{city.City}</div>
+            <div className="cityInfo">
+              <div>State: {city.State}</div>
+              <div>
+                Location: ({city.Lat}, {city.Long})
+              </div>
+              <div>Population (estimated): {city.EstimatedPopulation}</div>
+              <div>Total Wages: {city.TotalWages}</div>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  );
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="searchHeader">Header</div>
+        <div className="searchBox">
+          <form>
+            <label for="zipCode">Zip Code:</label>
+            <input type="number" name="zipCode" onChange={this.updateZip} />
+            <input type="submit" value="Search" onClick={this.handleSubmit} />
+          </form>
+        </div>
+        <div className="searchResults">{this.showCities()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
